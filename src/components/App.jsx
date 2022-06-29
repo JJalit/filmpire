dad626ea872e54836181f08b5f84d2b13e2755d1
jaeha <jaeha23@naver.com>
2022-06-29 21:27:26 +0900

    component structure & routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+
+import { Actors, MovieInformation, NavBar, Movies, Profile } from '.';
 
 function App() {
   return (
     <div>
-      App
+      <CssBaseline />
+      <NavBar />
+      <main>
+        <Routes>
+          <Route path="/movie/:id" element={<MovieInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/" element={<Movies />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
     </div>
   );
 }
